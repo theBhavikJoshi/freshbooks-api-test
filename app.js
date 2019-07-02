67c8cad7161a61ef84db0a66bdfba11fbf4dc0e8
BhavikJoshi <bhavik@getwalnut.com>
2019-07-03 00:39:55 +0530

    Excluded a Case from Incred Due SMS

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 var express = require("express");
+const axios = require('axios');
 var app = express();
 
 app.get("/redirect", (req, res, next) => {
-    console.log('Data: ', req);
+    // console.log('Data: ', req);
+    let headers = {
+        "Api-Version": "alpha",
+        "Content-Type": "application/json"
+    };
+    let data = {
+        "grant_type": "authorization_code",
+        "client_secret": "03b43e92ac0c72acf663319ae70b9fdf6334a100407661538c47a83de5ecec28",
+        "code": req.query.code,
+        "client_id": "c284839b801de0d32170d0f50812423aece00c807627d326ee86c3a4a3091e8c",
+        "redirect_uri": "https://enigmatic-woodland-76309.herokuapp.com/redirect"
+    }
+    let url = 'https://api.freshbooks.com/auth/oauth/token';
+    axios
+    .post(url, data, {headers: headers})
+    .then((response) => {
+        console.log('Response: ', response);
+    })
+    .catch((error) => {
+        console.log('Error: ', error);
+    })
     res.send('Hello');
 });
 
